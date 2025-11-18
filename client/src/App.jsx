fc81f178772c25f9f816fdd30cfde584141737fc
MahadevaPrasadMCS <mahadevaprasadcs23@gmail.com>
2025-11-18 08:01:39 +0530

    Added imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,7 +38,7 @@ import AdminAnnouncements from "./pages/admin/AdminAnnouncements";
 import ManageQuiz from "./pages/admin/ManageQuiz";
 import ManageStore from "./pages/admin/ManageStore";
 import UsersPage from "./pages/admin/UsersPage";
-import ProfileRequests from "./pages/admin/ProfileRequests";
+import ProfileRequests from "./pages/admin/AdminProfileRequests";
 import AdminLogs from "./pages/admin/AdminLogs";
 import AdminUsers from "./pages/admin/AdminUsers";
 import Rewards from "./pages/admin/Rewards";
