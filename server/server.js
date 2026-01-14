1f6971d54e66098d7800250ff34bfb2d9eb45655
MahadevaPrasadMCS <mahadevaprasadcs23@gmail.com>
2026-01-15 02:15:34 +0530

    Modified email sender

--- server/server.js
+++ server/server.js
@@ -25,7 +25,6 @@ app.set("trust proxy", 1);
 // CORS - FIXED
 // ==================================================
 const allowedOrigins = [
-  process.env.FRONTEND_URL,
   "http://localhost:3000",
   "https://youlearnhub-dp.vercel.app",
 ].filter(Boolean);
