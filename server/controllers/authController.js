522f51bdfba15c4c0901c63dbc20189116a5150b
MahadevaPrasadMCS <mahadevaprasadcs23@gmail.com>
2026-01-21 05:38:56 +0530

    added log

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -255,6 +255,7 @@ export const loginUser = async (req, res) => {
 // body: { otp, email, purpose }
 // -----------------------------
   export const verifyOTP = async (req, res) => {
+    console.log("VERIFY OTP:", { email, otp, purpose });
     try {
       const { otp, email, purpose = "REGISTER" } = req.body;
 
